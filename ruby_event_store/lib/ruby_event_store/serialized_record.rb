ec79ab5480a6eaebc8760d2b6e55b2ac7996be59
Paweł Pacana <pawel.pacana@gmail.com>
2019-10-08 15:42:33 +0200

    Extend SerializedRecord with timestamp.
    That helps repositories to separate it from metadata enabling better
    indexing and possibly filtering.

--- ruby_event_store/lib/ruby_event_store/serialized_record.rb
+++ ruby_event_store/lib/ruby_event_store/serialized_record.rb
@@ -3,16 +3,17 @@
 module RubyEventStore
   class SerializedRecord
     StringsRequired = Class.new(StandardError)
-    def initialize(event_id:, data:, metadata:, event_type:)
+    def initialize(event_id:, data:, metadata:, event_type:, timestamp:)
       raise StringsRequired unless [event_id, event_type].all? { |v| v.instance_of?(String) }
       @event_id   = event_id
       @data       = data
       @metadata   = metadata
       @event_type = event_type
+      @timestamp  = timestamp
       freeze
     end
 
-    attr_reader :event_id, :data, :metadata, :event_type
+    attr_reader :event_id, :data, :metadata, :event_type, :timestamp
 
     BIG_VALUE = 0b110011100100000010010010110011101011110101010101001100111110011
     def hash
@@ -22,6 +23,7 @@ module RubyEventStore
         data,
         metadata,
         event_type,
+        timestamp,
       ].hash ^ BIG_VALUE
     end
 
@@ -30,7 +32,8 @@ module RubyEventStore
         other.event_id.eql?(event_id) &&
         other.data.eql?(data) &&
         other.metadata.eql?(metadata) &&
-        other.event_type.eql?(event_type)
+        other.event_type.eql?(event_type) &&
+        other.timestamp.eql?(timestamp)
     end
 
     def to_h
@@ -39,6 +42,7 @@ module RubyEventStore
         data: data,
         metadata: metadata,
         event_type: event_type,
+        timestamp: timestamp,
       }
     end
 
